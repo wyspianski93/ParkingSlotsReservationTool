51bc9eaabd3f1732c520708918f558c39b841285
Dawid Wysocki <dawid.wysocki@ihsmarkit.com>
2023-10-20 01:17:46 +0200

    added message indicating no reservations for user slot

--- src/Web/web/src/components/reservations/user-slots/UserSlotReservations.tsx
+++ src/Web/web/src/components/reservations/user-slots/UserSlotReservations.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { useEffect } from "react";
 import { useRecoilRefresher_UNSTABLE, useRecoilValue } from "recoil";
 import { reservationsBySlotSelector } from "../../../state/reservationsState";
@@ -13,9 +14,11 @@ export function UserSlotReservations({ slotId }: { slotId: string }): JSX.Elemen
 
   return (
     <>
-      {reservations.map((reservation) => (
-        <UserSlotReservation reservation={reservation} />
-      ))}
+      {reservations.length > 0 &&
+        reservations.map((reservation) => <UserSlotReservation reservation={reservation} />)}
+      {reservations.length === 0 && (
+        <Typography variant="h4">You don't have any reservations for this slot.</Typography>
+      )}
     </>
   );
 }
